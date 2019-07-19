eeed553d8066b5cb207795b145e652c35ffd277d
Andrey Lushnikov <lushnikov@chromium.org>
2019-07-18 18:59:21 -0700

    trigger build

---
+++ a.js
@@ -0,0 +1,17 @@
+const exec = require('child_process').exec;
+const pptr = require('puppeteer-core');
+
+const q = require('./chrome.json');
+console.log(JSON.stringify(q, null, 2));
+
+exec(`"${q.executablePath}" --version`, async function(error, stdout, stderr) {
+  console.log(error);
+  console.log(stdout);
+  console.log('--------');
+  const browser = await pptr.launch(q);
+  console.log(browser.version());
+  await browser.close();
+});
+
+
+
